3a9a52a46dfc30031821865c9025c09c88d5c488
Marcelo Garlet Millani <marcelogmillani@gmail.com>
2017-04-02 15:28:17 +0200

    License note on files

--- src/Pictikz/Drawing.hs
+++ src/Pictikz/Drawing.hs
@@ -1,3 +1,19 @@
+--  Copyright 2017 Marcelo Garlet Millani
+--  This file is part of pictikz.
+
+--  pictikz is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+
+--  pictikz is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+
+--  You should have received a copy of the GNU General Public License
+--  along with pictikz.  If not, see <http://www.gnu.org/licenses/>.
+
 module Pictikz.Drawing where
 
 class Drawable a where
