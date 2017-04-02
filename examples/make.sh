a45027d8216c4515529bfbfcd0f27ff3b34beaf0
Marcelo Garlet Millani <marcelogmillani@gmail.com>
2017-04-02 15:26:35 +0200

    Examples

---
+++ examples/make.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Grid example
+pictikz           --fit 3 3 grid.svg --output grid-plain.tex
+pictikz --uniform --fit 3 3 grid.svg --output grid-uniform.tex
+pictikz --grid    --fit 3 3 grid.svg --output grid-grid.tex
+
+# Tree example
+pictikz           --fit 2.5 2.5 tree.svg --output tree-plain.tex
+pictikz --uniform --fit 2.5 2.5 tree.svg --output tree-uniform.tex
+pictikz --grid    --fit 2.5 2.5 tree.svg --output tree-grid.tex
+
+# Star example
+pictikz           --scale 2.5 2.5 star.svg --output star-plain.tex
+pictikz --uniform --scale 2.5 2.5 star.svg --output star-uniform.tex
+pictikz --grid    --scale 2.5 2.5 star.svg --output star-grid.tex
+
+# Stickman example
+pictikz              --fit 3 3 running-man.svg --output running-man-plain.tex
+pictikz --uniform 10 --fit 3 3 running-man.svg --output running-man-uniform.tex
+pictikz --grid    10 --fit 3 3 running-man.svg --output running-man-grid.tex
+
+# Box example
+pictikz           --scale 2 2 box.svg --output box-plain.tex
+pictikz --uniform --scale 2 2 box.svg --output box-uniform.tex
+pictikz --grid    --scale 2 2 box.svg --output box-grid.tex
+
+pdflatex examples.tex
