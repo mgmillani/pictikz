af6e06ee9176ebf6bb39e1871792b3128fa3401e
Marcelo Garlet Millani <marcelogmillani@gmail.com>
2017-11-09 18:35:40 +0100

    Easily increase version.

--- next-version.sh
+++ next-version.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-if [[ $# -eq 1 ]]
+if [[ $# -eq 0 ]]
 then
 	echo "Please provide version number."
 	exit 1
@@ -19,14 +19,17 @@ cat "pictikz.cabal" | sed "s/^version:\( *\)[\"0-9\\.]*/version:\1$VERSION/" > "
 cp $TEMP "pictikz.cabal"
 
 # Changelog
-echo "# Revision history for pictikz" > "$TEMP"
+echo "# Revision history for pictikz
+" > "$TEMP"
 echo "## $1 -- $TODAY" >> "$TEMP"
 tail -n +3 ChangeLog.md >> "$TEMP"
+cp "$TEMP" ChangeLog.md
 
 # man page
 cd docs
-cat man.header | sed 's:\\.TH\([^"]*\)"[^"]*".*\("[^"]*man page"\):.TH\1"'$TODAY'" "'$VERSION'" \2': > "$TEMP"
+cat man.header | sed 's:\.TH\([^"]*\)"[^"]*".*\("[^"]*man page"\):.TH\1"'"$TODAY"'" "'"$VERSION"'" \2:' > "$TEMP"
 cp "$TEMP" man.header
+
 make
 cp pictikz.1 ../
 cp README.md ../
